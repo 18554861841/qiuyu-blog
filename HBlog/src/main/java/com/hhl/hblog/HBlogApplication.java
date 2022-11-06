03858ab3cd2218b26107d202920cc635bbe0b506
hhl <hanhailong0516@163.com>
2022-11-06 20:15:47 +0800

    创建项目

---
+++ HBlog/src/main/java/com/hhl/hblog/HBlogApplication.java
@@ -0,0 +1,13 @@
+package com.hhl.hblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HBlogApplication.class, args);
+    }
+
+}
